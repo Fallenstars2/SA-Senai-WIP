1be95978d2688d59e153f32057ffc235c10a2d8a
Fallenstars <165226924+Fallenstars2@users.noreply.github.com>
2024-07-23 20:40:02 -0300

    Add files via upload

---
+++ cadastropessoa final/script.js
@@ -0,0 +1,8 @@
+function cadastro(){
+    document.getElementsByClassName('cadastrodiv')[0].style.display = 'block'
+    document.getElementsByClassName('logindiv')[0].style.display = 'none'
+}
+function voltar(){
+    document.getElementsByClassName('cadastrodiv')[0].style.display = 'none'
+    document.getElementsByClassName('logindiv')[0].style.display = 'block'
+}
