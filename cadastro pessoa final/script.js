4893639ba30174917b89cf20790b9c60a048b0bb
Fallenstars <165226924+Fallenstars2@users.noreply.github.com>
2024-07-29 15:33:48 -0300

    Add files via upload

---
+++ cadastro pessoa final/script.js
@@ -0,0 +1,8 @@
+function cadastro(){
+    document.getElementsByClassName('cadastrodiv')[0].style.display = 'block'
+    document.getElementsByClassName('loginpage')[0].style.display = 'none'
+}
+function voltar(){
+    document.getElementsByClassName('cadastrodiv')[0].style.display = 'none'
+    document.getElementsByClassName('loginpage')[0].style.display = 'block'
+}
